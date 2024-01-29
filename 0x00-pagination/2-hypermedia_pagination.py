d74f383745c2db32f46d0f34631b59602d2830f4
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-30 00:29:15 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Hypermedia pagination"""
-
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
 class Server:
@@ -25,24 +24,36 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page, page_size) -> Tuple[int, int]:
+        """
+        Takes two integer arguments page and page_size
+        Returns a tuple
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+        """Get Page"""
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
+        start, end = self.index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
 
-
-def index_range(page, page_size):
-    """
-    Takes two integer arguments page and page_size
-    Returns a tuple
-    """
-    return 0
-
-
-def get_page(page=1, page_size=10):
-    """Get Page"""
-    return 0
-
-
-
-def get_hyper(page=1, page_size=10):
-    """Hyper pagination"""
-    return 0
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Get hyper"""
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 <= total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
