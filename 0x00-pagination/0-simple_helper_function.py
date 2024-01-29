49908032ea6d0ee68e041ac9bbeb9473e0566eac
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-29 22:15:14 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Simple helper function"""
+
+def index_range(page, page_size):
+    """
+    Takes two integer arguments page and page_size
+    Returns a tuple
+    """
+    return 0
