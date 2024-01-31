429ce4f6615d2ceb1621abf0a320a6c08f6f8edd
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-01-31 20:45:24 +0300

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" BasicCache module """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class that inherits from BaseCaching """
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
