7ca3cd6058d7418d3f7a6a9f770f92b33ded6f73
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-09 22:27:32 +0300

    Mock Logging

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,50 +1,69 @@
 #!/usr/bin/env python3
+"""Mock Logging"""
 
-"""
-Mock Logging
-"""
-from flask import Flask, render_template, g, request
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
 app = Flask(__name__)
+babel = Babel(app)
 
-# User table
-USERS = {
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-# Define get_user function
 
-
-def get_user(user_id):
-    """get user"""
-    return USERS.get(user_id)
-
-# Define before_request function
+def get_user(login_as):
+    """
+    get_user
+    """
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return None  # Explicitly return None if an exception occurs
 
 
 @app.before_request
 def before_request():
-    login_as = request.args.get('login_as')
-    if login_as:
-        g.user = get_user(int(login_as))
-    else:
-        g.user = None
-
-# Define route
+    """
+    before_request
+    """
+    g.user = get_user(request.args.get("login_as"))
 
 
-@app.route('/')
-def index():
-    """index"""
-    welcome_message = ""
-    if g.user:
-        welcome_message = f"You are logged in as {g.user['name']}."
-    else:
-        welcome_message = "You are not logged in."
-    return render_template('index.html', welcome_message=welcome_message)
+@babel.localeselector
+def get_locale():
+    """
+    get_locale
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
+    """
+    hello
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
